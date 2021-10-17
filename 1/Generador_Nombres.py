ce23cea34d7f5ea23731a555311cd976463b9a10
EmmanuelNavarrete <s18030188@cdhidalgo.tecnm.mx>
2021-10-17 17:14:29 -0500

    Create Generador_Nombres.py

---
+++ 1/Generador_Nombres.py
@@ -0,0 +1,8 @@
+# Este es un generador de nombres lo cual toma dos String
+# Y despues los junta
+
+print("Bienvenido al generador de nombres de bandas de rock")
+nom_ciudad = (input("Cual es el nombre la ciudad donde naciste? \n"))
+nom_mascota = (input("Cual es el nombre de tu mascota? \n"))
+print("El nombre de la banda será ", nom_ciudad + "" + nom_mascota)
+
